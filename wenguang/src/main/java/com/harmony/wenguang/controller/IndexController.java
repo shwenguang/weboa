9e89ec9b611bf124c1414cbb1e8c10126f30870b
yin <yglah126@126.com>
2017-06-20 20:59:07 +0800

    mesage

--- wenguang/src/main/java/com/harmony/wenguang/controller/IndexController.java
+++ wenguang/src/main/java/com/harmony/wenguang/controller/IndexController.java
@@ -2,16 +2,18 @@ package com.harmony.wenguang.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/index")
 public class IndexController {
 	
-	@RequestMapping(value="/ii")
-	public ModelAndView index(){
+	@RequestMapping(value="/hello")
+	@ResponseBody
+	public String index(){
 		ModelAndView mv = new ModelAndView("hello");
 		System.out.println(">>>>>>>ssss>>>>>>");
-		return mv;
+		return "hello world";
 	}
 }
