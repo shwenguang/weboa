fdfc8ff28fa2537bc690381ae6fa498b9eabffcc
yin <yglah126@126.com>
2017-06-20 13:52:34 +0800

    doc2html

---
+++ wenguang/src/main/java/com/harmony/wenguang/support/Word2Html.java
@@ -0,0 +1,96 @@
+package com.harmony.wenguang.support;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.converter.PicturesManager;
+import org.apache.poi.hwpf.converter.WordToHtmlConverter;
+import org.apache.poi.hwpf.usermodel.Picture;
+import org.apache.poi.hwpf.usermodel.PictureType;
+import org.apache.poi.xwpf.converter.core.FileImageExtractor;
+import org.apache.poi.xwpf.converter.core.FileURIResolver;
+import org.apache.poi.xwpf.converter.xhtml.XHTMLConverter;
+import org.apache.poi.xwpf.converter.xhtml.XHTMLOptions;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.w3c.dom.Document;
+
+import com.google.common.io.Files;
+
+public class Word2Html {
+	private static String picPath = "D:/alidata1/pics/";
+//    public static void main(String args[]) throws Exception{
+//        convertToHtml("D:/alidata1/origindoc.doc","D:/alidata1/origindoc.html");
+//        convertToHtml("D:/alidata1/origindoc.docx","D:/alidata1/origindocx.html");
+//    }
+    
+    public static void convertToHtml(String docFile,String htmlFile) throws Exception{
+        if(docFile.endsWith("docx")){
+            docx2Html(docFile, htmlFile);
+        }else{
+            doc2html(docFile,htmlFile);
+        }
+    }
+    
+    public static void doc2html(String docFile,String htmlFile) throws Exception{
+        HWPFDocument doc = new HWPFDocument(new FileInputStream(docFile));
+        WordToHtmlConverter wordToHtmlConverter = new WordToHtmlConverter(DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument());
+        wordToHtmlConverter.setPicturesManager(new PicturesManager(){
+            public String savePicture(byte[] content, 
+                                      PictureType pictureType, 
+                                      String suggestedName, 
+                                      float widthInches,
+                                      float heightInches) {
+                return "test/"+suggestedName;
+            }});
+        
+        wordToHtmlConverter.processDocument(doc);
+        
+        List<Picture> pics = doc.getPicturesTable().getAllPictures();
+        if(pics!=null){
+            for(Picture pic : pics){
+                System.out.println(pic.suggestFullFileName());
+            }
+        }
+        
+        Document htmlDoc = wordToHtmlConverter.getDocument();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        DOMSource domSource = new DOMSource(htmlDoc);
+        StreamResult streamResult = new StreamResult(out);
+        
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer trans = tf.newTransformer();
+        
+        trans.setOutputProperty(OutputKeys.ENCODING, "utf-8");
+        trans.setOutputProperty(OutputKeys.INDENT, "yes");
+        trans.setOutputProperty(OutputKeys.METHOD, "html");
+        trans.transform(domSource, streamResult);
+        out.close();
+        
+        Files.write(out.toByteArray(), new File(htmlFile));
+        
+    }
+    
+    public static void docx2Html(String docFile,String htmlFile) throws Exception{
+        XWPFDocument doc = new XWPFDocument(new FileInputStream(docFile));
+        XHTMLOptions options = XHTMLOptions.create().indent(4);
+        File imgFolder = new File(picPath);
+        options.setExtractor(new FileImageExtractor(imgFolder));
+        options.URIResolver(new FileURIResolver(imgFolder));
+        
+        OutputStream out = new FileOutputStream(new File(htmlFile));
+        XHTMLConverter.getInstance().convert(doc, out, options);
+        System.out.println("docx to html done");
+    }
+}
