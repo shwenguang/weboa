dc791ae0b0b21001bf843bef55bbc3b2925d431c
yin <yglah126@126.com>
2017-06-25 23:12:35 +0800

    xxxxx

--- wenguang/src/main/java/com/harmony/wenguang/controller/HtmlController.java
+++ wenguang/src/main/java/com/harmony/wenguang/controller/HtmlController.java
@@ -1,6 +1,7 @@
 package com.harmony.wenguang.controller;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +14,17 @@ import com.harmony.wenguang.service.HtmlService;
 public class HtmlController {
 	@Resource
 	HtmlService htmlService;
+	@Resource
+	HttpServletRequest request;
 	
 	@RequestMapping("/doc.do")
 	@ResponseBody
 	public String html(){
+		String path1 = request.getParameter("path1");
+		String path2 = request.getParameter("path2");
+		String path3 = request.getParameter("path3");
+		
 		System.out.println("htmlService=="+htmlService);
-		return htmlService.getHtml(null);
+		return htmlService.getHtml(null) + ",path1="+path1+",path2 = "+ path2 + ", path3 = "+path3;
 	}
 }
