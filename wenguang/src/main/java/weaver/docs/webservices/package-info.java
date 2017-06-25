069a65db26a94075322abb8733c480d4faca2ecf
yin <yglah126@126.com>
2017-06-25 13:41:09 +0800

    sssss

--- wenguang/src/main/java/weaver/docs/webservices/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://localhost/services/DocService", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package weaver.docs.webservices;
